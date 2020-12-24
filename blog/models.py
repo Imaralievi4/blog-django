6769cdacea1abb13485b53eefcf0c3bc7dc1768b
Imaralievi4 <zkd3year@gmail.com>
2020-12-25 02:31:01 +0600

    blog-django

---
+++ blog/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, primary_key=True)
+
+    def __str__(self):
+        return self.title 
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.CASCADE, 
+        related_name='posts'
+        )
+    image = models.ImageField(
+        upload_to='posts', 
+        null=True, 
+        blank=True
+        )
+    author = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.CASCADE, 
+        related_name='posts'
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+
+    # userobject.posts.all()
+    # userobject.posts_set.all()
+    # questionobj.choice_set.all()
